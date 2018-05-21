873c554e0f1dcd34c179df5961c34fd1a43f288c
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2018-05-21 15:25:35 +0200

    Added code

---
+++ app.js
@@ -0,0 +1,6 @@
+var stuff = require("./stuff");
+
+console.log(stuff.counter(["shaun", "crystal", "ryu"]));
+console.log(stuff.adder(5, 6));
+console.log(stuff.pi);
+console.log(stuff.adder(stuff.pi, 4));
