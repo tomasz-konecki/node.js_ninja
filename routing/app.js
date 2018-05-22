6de688f222f574f248cb19a1247c1f379601f903
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2018-05-22 13:05:57 +0200

    Update

--- routing/app.js
+++ routing/app.js
@@ -3,8 +3,9 @@ var fs = require("fs");
 
 var server = http.createServer(function(req, res) {
   console.log("request was made: " + req.url);
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("hello there...");
+  if(req.url === '/home' || req.url === '/'){
+      res.writeHead(200, {'Content-Type'})
+  }
 });
 
 server.listen(3000, "127.0.0.1");
